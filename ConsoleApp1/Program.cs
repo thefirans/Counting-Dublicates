600cc6bb8dfd0a5b62aaa52218eb9f02d3679058
Sasha <sleo170504@gmail.com>
2022-10-15 18:11:37 +0300

    add comments

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,8 +12,12 @@ namespace ConsoleApp1
     {
         public static int DuplicateCount(string str)
         {
-            var result = str.ToLower().Replace(" ", "").GroupBy(_ => _).Where(x => x.Count() > 1).Select(x => x.Key);
-            return result.Count();    
+            var result = str.ToLower()      //ToLower() for ignore case
+                .Replace(" ", "")           //Replace() for delete all whitespaces
+                .GroupBy(_ => _)            //GroupBy() for split all characters to single char
+                .Where(x => x.Count() > 1)  //Where() for find dublicates, i.e. count char's that we have more than one time
+                .Select(x => x.Key);        //Select() group all characters into small groups
+            return result.Count();          //Count() count and return biggest group
         }
     }
 }
